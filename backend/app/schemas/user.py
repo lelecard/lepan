8b827864d6f90a3f200de66e7dbdb3405a05ebe8
lelecard <zhuangyongda.v@gmail.com>
2026-09-22 07:28:11 +0800

    Create user.py

---
+++ backend/app/schemas/user.py
@@ -0,0 +1,31 @@
+from pydantic import BaseModel
+
+
+class UserCreate(BaseModel):
+
+    email: str
+
+    password: str
+
+
+
+class UserLogin(BaseModel):
+
+    email: str
+
+    password: str
+
+
+
+class UserResponse(BaseModel):
+
+    id: int
+
+    email: str
+
+    plan: str
+
+
+    class Config:
+
+        from_attributes = True
