4c50691990f495737ac2121e25a8deea931f877e
lelecard <zhuangyongda.v@gmail.com>
2026-09-22 07:41:14 +0800

    Update users.py

--- backend/app/api/users.py
+++ backend/app/api/users.py
@@ -1,8 +1,15 @@
 from fastapi import APIRouter
+from fastapi import Depends
+
+from sqlalchemy.orm import Session
+
+
+from database import get_db
 
 from schemas.user import UserCreate
 
 from services.user_service import create_user
+
 
 
 router = APIRouter(
@@ -17,15 +24,31 @@ router = APIRouter(
 
 @router.post("/register")
 def register(
-    user: UserCreate
+
+    user: UserCreate,
+
+    db: Session = Depends(get_db)
+
 ):
+
+
+    new_user = create_user(
+
+        db,
+
+        user.email,
+
+        user.password
+
+    )
 
 
     return {
 
-        "email": user.email,
+        "id": new_user.id,
 
-        "message":
-        "User created"
+        "email": new_user.email,
+
+        "plan": new_user.plan
 
     }
