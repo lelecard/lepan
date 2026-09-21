c744282606cf2e22f0345b51788c0c7a456afd56
lelecard <zhuangyongda.v@gmail.com>
2026-09-22 07:37:45 +0800

    Update users.py

--- backend/app/api/users.py
+++ backend/app/api/users.py
@@ -1,19 +1,31 @@
 from fastapi import APIRouter
 
+from schemas.user import UserCreate
+
+from services.user_service import create_user
+
 
 router = APIRouter(
+
     prefix="/users",
+
     tags=["Users"]
+
 )
 
 
 
 @router.post("/register")
-def register():
+def register(
+    user: UserCreate
+):
+
 
     return {
 
+        "email": user.email,
+
         "message":
-        "User registration API ready"
+        "User created"
 
     }
